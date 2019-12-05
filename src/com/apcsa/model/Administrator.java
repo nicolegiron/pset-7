d3513e848ee61dfc0cd2bd0305c74f03a0732fb8
nicolegiron1 <ngiron@ucvts.org>
2019-12-05 09:22:57 -0500

    so confusion, trying things out

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -7,6 +7,10 @@ import com.apcsa.model.User;
 
 public class Administrator extends User {
 
+	public Administrator(User user, ResultSet rs) throws SQLException {
+		super(rs);
+	}
+	
 	private int administratorId;
     private String firstName;
     private String lastName;
