d292ad3d986cec1019cdbbb90cccda758bbed636
nicolegiron1 <ngiron@ucvts.org>
2019-12-05 09:01:52 -0500

    original

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -1,12 +1,15 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.apcsa.model.User;
 
 public class Administrator extends User {
 
-    private int administratorId;
+	private int administratorId;
     private String firstName;
     private String lastName;
     private String jobTitle;
-
+    
 }
--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,10 +1,12 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
 import com.apcsa.model.User;
 
 public class Teacher extends User {
 
-    private int teacherId;
+	private int teacherId;
     private int departmentId;
     private String firstName;
     private String lastName;
